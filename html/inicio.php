41f3aa334beda31091405800fa71dd04b4de0668
Ignacio Fernandez <fernandezignacio2000@gmail.com>
2023-11-30 10:51:55 +0100

    agrupacion de contenido

--- html/inicio.php
+++ html/inicio.php
@@ -1,21 +1,20 @@
-
 <?php
 session_start();
 
 
 // Verifica si el usuario no ha iniciado sesión
 if (!isset($_SESSION['nombre_usuario'])) {
-    header('Location: login.php');
-    exit();
+  header('Location: login.php');
+  exit();
 }
 
 
 // Verifica si se ha enviado el formulario
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['cerrar_sesion'])) {
-    // Destruye la sesión y redirige al inicio de sesión
-    session_destroy();
-    header('Location: login.php');
-    exit();
+  // Destruye la sesión y redirige al inicio de sesión
+  session_destroy();
+  header('Location: login.php');
+  exit();
 }
 ?>
 <!doctype html>
@@ -35,12 +34,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['cerrar_sesion'])) {
   <!-- Bootstrap JavaScript Libraries -->
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
     integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
-  </script>
+    </script>
 
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
     integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
-  </script>
+    </script>
 </head>
 
 
@@ -49,14 +48,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['cerrar_sesion'])) {
     <!-- place navbar here -->
   </header>
   <main>
-  <h2>Bienvenido, <?php echo $_SESSION['nombre_usuario']; ?>!</h2>
+    <h2>Bienvenido,
+      <?php echo $_SESSION['nombre_usuario']; ?>!
+    </h2>
     <form method="post" action="">
-        <input type="submit" name="cerrar_sesion" value="Cerrar Sesión">
+      <input type="submit" name="cerrar_sesion" value="Cerrar Sesión">
     </form>
     <!-- Tu contenido aquí -->
     <div style="padding: 16px;">
-        <h2>Bienvenido a tu banca online de confianza </h2>
-        <p>Contenido de la página...</p>
+      <h2>Bienvenido a tu banca online de confianza </h2>
+      <p>Contenido de la página...</p>
     </div>
   </main>
   <footer>
@@ -68,6 +69,3 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['cerrar_sesion'])) {
 
 
 </html>
-
-
-
