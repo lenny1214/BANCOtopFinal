156b3550539a8452ab95ec40ac216f5d20a9d49b
lenny1214 <fernandezignacio0000@gmail.com>
2023-12-14 08:55:16 +0100

    tocandocosas

--- html/anadiringa.php
+++ html/anadiringa.php
@@ -77,6 +77,7 @@ $conn->close();
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
             integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous"></script>
     <script>
+
         function mostrarMensajeExito(mensaje) {
             // Eliminar mensajes anteriores
             var mensajesAnteriores = document.querySelectorAll(".alert");
@@ -94,6 +95,8 @@ $conn->close();
             formulario.parentNode.insertBefore(mensajeDiv, formulario);
         }
     </script>
+  
+    
 </head>
 
 <body>
